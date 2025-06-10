21fd6f7e8bf49abc53e9f0b6b0995c88e8f5c9b8
libertino004 <libertinosarmento004@gmail.com>
2025-06-10 12:35:33 +0700

    Add files via upload

---
+++ spam_detector.py
@@ -0,0 +1,17 @@
+import joblib
+import string
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\d+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+def load_model():
+    return joblib.load("models/spam_classifier_model.pkl"), joblib.load("models/vectorizer.pkl")
+
+def predict_spam(text, model, vectorizer):
+    cleaned = clean_text(text)
+    vect_text = vectorizer.transform([cleaned])
+    return model.predict(vect_text)[0]
